9974e4739565e3d7d13d720fd473f9f1ee130657
mdrayhan124 <ranjerrayhan124@gmail.com>
2024-02-18 04:03:23 +0600

    all new

---
+++ src/components/Home.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import HeroImage from '../assets/heroImage.png'
+import { FaArrowRight } from "react-icons/fa";
+
+const Home = () => {
+    return (
+        <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 '>
+            <div className=' max-w-screen-lg mx-auto flex flex-col justify-center items-center h-full px-4 md:flex-row'>
+                <div className='flex flex-col justify-center h-full'>
+                    <h1 className='text-4xl sm:text-7xl text-white font-bold'>I'm a Full Stack Developer</h1>
+                    <p className='py-4 text-gray-500 max-w-md '>I have 8 years of experience building and desgining software.
+                        Currently, I love to work on web application using technologies like
+                        React, Tailwind, Next JS and GraphQL.</p>
+                    <button className=' group text-white px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 w-fit cursor-pointer gap-3'>
+                        Portfolio
+                        <span className='group-hover:rotate-90 duration-300'>
+                            <FaArrowRight />
+                        </span>
+                    </button>
+                </div>
+                <div>
+                    <img src={HeroImage} alt="my-profile"  className='rounded-2xl w-2/3 md:w-full'/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react'
+import { FaBars, FaTimes } from "react-icons/fa";
+
+const NavBar = () => {
+
+    const [nav,setNav]=useState(false)
+
+    const links = [
+        {
+            id: 1,
+            link: "home"
+        },
+        {
+            id: 2,
+            link: "about"
+        },
+        {
+            id: 4,
+            link: "portfolio"
+        },
+        {
+            id: 4,
+            link: "experienses"
+        },
+        {
+            id: 3,
+            link: "contact"
+        },
+
+    ]
+
+    return (
+        <div className='flex justify-between items-center text-white bg-black fixed w-full h-20 px-4'>
+            <div>
+                <h1 className='font-signature text-5xl'>yash</h1>
+            </div>
+            <ul className='hidden md:flex'>
+                {
+                    links.map(({ id, link }) => (
+                        <li key={id} className='pl-4 text-gray-500 cursor-pointer hover:scale-105 hover:text-white duration-200 capitalize '>{link}</li>
+                    ))
+                }
+            </ul>
+
+            <div onClick={()=>setNav(!nav)} className='text-gray-500 z-10 md:hidden'>
+                {
+                    nav?<FaTimes size={30}/>:<FaBars size={30}/>
+                }
+            </div>
+            {
+                nav&&(
+                    <ul className='flex flex-col justify-center items-center w-full h-screen absolute top-0 left-0 bg-gradient-to-b from-black to-gray-800 text-gray-500'>
+            {
+                    links.map(({ id, link }) => (
+                        <li key={id} className='px-4 cursor-pointer capitalize text-4xl py-6 hover:text-white'>{link}</li>
+                    ))
+                }
+            </ul>
+                )
+            }
+           
+        </div>
+    )
+}
+
+export default NavBar
